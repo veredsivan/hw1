e1458a56c52d6e9150a9a9e1c60164537c0372c2
boazfre <130273530+boazfre@users.noreply.github.com>
2023-06-18 20:10:29 +0300

    Update firewall.sh after G

--- boazveredupdated2/firewall.sh
+++ boazveredupdated2/firewall.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 #stors the input from stdin in packets variable 
-read packets_file | sort | uniq
-for packet in $packets_file
+read packets_file
+sorted_packets=$(echo "$packets_file" | sort | uniq)
+
+for packet in $sorted_packets
 do
-	rules_file=$(cat $0)
+	rules_file=$(cat "$0")
 	for rule in $rules_file
 	do
 		flag=0
-		sed -i '/#/d' rule #removes all comment lines
-		num_command=$(($(grep -o ',' rule | wc -l) + 1))
+		sed -i '/#/d' "$rule" #removes all comment lines
+		num_command=$(($(grep -o ',' "$rule" | wc -l) + 1))
 		for i in $num_command
 		do
 			awk -F, '{command=$(i)}'
-			compare=cat packet|./firewall.exe "$command"
-			if ["$packet" -neq "$compare" ] 
+			compare=$(cat packet|./firewall.exe "$command")
+			if [["$packet" -neq "$compare" ]] 
 			then
 				break # cause command in rule faild, moving to the next rule 
 			else 
 				((flag++))
 			fi
 		done
-		if [flag -eq $num_command] 
+		if [[$flag -eq $num_command]] 
 		then 
-			echo $packet # print out the packet that maches the rule
+			echo "$packet" # print out the packet that maches the rule
 			break # cause rule succsses, moving to the next packet
 		fi
 	done
