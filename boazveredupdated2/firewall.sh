0877fc774e4a3ebd070ce1c3b9a6946adbfc6a29
boazfre <130273530+boazfre@users.noreply.github.com>
2023-06-18 20:13:54 +0300

    Update firewall.sh GP

--- boazveredupdated2/firewall.sh
+++ boazveredupdated2/firewall.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
-#stors the input from stdin in packets variable 
+# Store the input from stdin in packets_file variable
 read packets_file
 sorted_packets=$(echo "$packets_file" | sort | uniq)
 
-for packet in $sorted_packets
-do
-	rules_file=$(cat "$0")
-	for rule in $rules_file
-	do
-		flag=0
-		sed -i '/#/d' "$rule" #removes all comment lines
-		num_command=$(($(grep -o ',' "$rule" | wc -l) + 1))
-		for i in $num_command
-		do
-			awk -F, '{command=$(i)}'
-			compare=$(cat packet|./firewall.exe "$command")
-			if [["$packet" -neq "$compare" ]] 
-			then
-				break # cause command in rule faild, moving to the next rule 
-			else 
-				((flag++))
-			fi
-		done
-		if [[$flag -eq $num_command]] 
-		then 
-			echo "$packet" # print out the packet that maches the rule
-			break # cause rule succsses, moving to the next packet
-		fi
-	done
+for packet in $sorted_packets; do
+    rules_file=$(cat "$0")
+    for rule in $rules_file; do
+        flag=0
+        sed -i '/#/d' "$rule" # Removes all comment lines
+        num_command=$(($(grep -o ',' "$rule" | wc -l) + 1))
+        
+        for ((i = 1; i <= num_command; i++)); do
+            awk -F, '{command=$i; print command}'
+            compare=$(cat "$packet" | ./firewall.exe "$command")
+            
+            if [[ "$packet" != "$compare" ]]; then
+                break # Command in rule failed, moving to the next rule
+            else
+                ((flag++))
+            fi
+        done
+        
+        if [[ $flag -eq $num_command ]]; then
+            echo "$packet" # Print out the packet that matches the rule
+            break # Rule succeeded, moving to the next packet
+        fi
+    done
 done
